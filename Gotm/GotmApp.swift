275a399f797127f710df0c5f7ad1e8305430c65a
supr <suprit@315.am>
2026-03-20 02:21:51 +0530

    Initial Commit

---
+++ Gotm/GotmApp.swift
@@ -0,0 +1,17 @@
+//
+//  GotmApp.swift
+//  Gotm
+//
+//  Created by Suprit Parulkar on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GotmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GotmTests/GotmTests.swift
@@ -0,0 +1,16 @@
+//
+//  GotmTests.swift
+//  GotmTests
+//
+//  Created by Suprit Parulkar on 20/03/26.
+//
+
+import Testing
+
+struct GotmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
